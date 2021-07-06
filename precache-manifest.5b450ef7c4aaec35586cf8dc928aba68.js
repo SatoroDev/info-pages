8e61fc34a9a0b8a2f79d62421ecbf771a9c91519
SatoroDEV <satoroswap@gmail.com>
2021-07-06 17:38:56 +0200

    new UI

---
+++ precache-manifest.5b450ef7c4aaec35586cf8dc928aba68.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5c7419081b295e07f2baf6d3352d1e3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7aaf8fdb27c1a7bda160",
+    "url": "/static/js/2.db9d432b.chunk.js"
+  },
+  {
+    "revision": "8efadbe79de939f183de4c88d2491df0",
+    "url": "/static/js/2.db9d432b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8d85e110a8242c01b99",
+    "url": "/static/js/main.1a104497.chunk.js"
+  },
+  {
+    "revision": "551c9e280db88d76f240",
+    "url": "/static/js/runtime-main.c307dd36.js"
+  },
+  {
+    "revision": "73dabb371ddce7c640334fe0904dcddf",
+    "url": "/static/media/eth.73dabb37.png"
+  },
+  {
+    "revision": "c1d2a292d78d23364b4d37ab5a97336a",
+    "url": "/static/media/logo-black.c1d2a292.png"
+  },
+  {
+    "revision": "ad159c31c550363d002861281f7586f0",
+    "url": "/static/media/logo.ad159c31.png"
+  }
+]);
